c550584b2a540333a3e41b061d52ebe218a52901
jams009vasoya <91665696+jams009vasoya@users.noreply.github.com>
2021-09-30 15:43:19 +0530

    Add files via upload

---
+++ letupgrade/day.py
@@ -0,0 +1,33 @@
+x = 2
+y = 5
+xy = 'Hey'
+print (x+y, xy)
+
+print("""My name is JAYMIN VASOYA
+
+I love Python.""")
+
+string1 = 'World'
+print('Hello', string1)
+
+string2 = '!'
+print('Hello', string1, string2)
+
+print('Hello' + string1 + string2)
+
+print("Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug")
+
+print("I want \\n to be printed.")
+
+x = 10
+print(type(x))
+
+s = 'abc'
+print(type(s))
+
+value= True
+print(type(value))
+
+percent=90.00
+type(percent)
+
